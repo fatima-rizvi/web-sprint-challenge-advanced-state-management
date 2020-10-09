091151e7a24ec4852ef1898ab893330c304fcdc2
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-09 10:09:12 -0500

    npm install in the root folder and added midleware to index.js in src

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import createStore and applyMiddleware
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
 
