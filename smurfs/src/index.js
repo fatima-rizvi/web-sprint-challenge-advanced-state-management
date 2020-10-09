77fdbd632c1e2a42dc4afd97cb21797cb58d2380
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-09 10:26:48 -0500

    still waiting for npm install to run in the app, set up index.js and the basic reducer in the meantime

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,7 +4,18 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+
+//import reducer 
+import { reducer } from './reducers/smurfReducer';
+
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//add applyMiddleware to app
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,14 @@
+// set up initial state
+const initialState = {
+    smurfs = [],
+    isLoading: false,
+    error: '',
+    newSmurf = {}
+};
+
+export const reducer = ( state = initialState, action) => {
+    switch( action.type ){
+        default:
+            return state;
+    };
+};
